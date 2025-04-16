79627d8fe3c8679364e7c1d44fdfd6e89e3e0ce7
TmrBotz <tmrbotz@gmail.com>
2025-04-16 11:15:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const app = express();
 
 const bot = new Telegraf("7861502352:AAHnJW2xDIZ6DL1khVo1Hw4mXvNYG5pa4pM"); // Replace with your bot token
 const CHANNEL_ID = "-1001991464977"; // Replace with your private channel ID
-
 const fileStorage = {};
 
 function generateRandomId() {
@@ -19,19 +18,11 @@ bot.start(async (ctx) => {
     const randomId = text.split(" ")[1];
     if (fileStorage[randomId]) {
       const fileData = fileStorage[randomId];
-
-      const extraOptions = {
-        caption: fileData.caption || '',
-        parse_mode: "HTML"
-      };
-
-      if (fileData.file_name) {
-        extraOptions.filename = fileData.file_name;
-      }
-
       return await ctx.replyWithDocument(
-        { file_id: fileData.file_id, filename: fileData.file_name },
-        extraOptions
+        fileData.file_id,
+        {
+          caption: fileData.caption || undefined
+        }
       );
     } else {
       return ctx.reply("Invalid or expired link.");
@@ -49,52 +40,31 @@ bot.start(async (ctx) => {
 
 bot.on(['document', 'photo', 'video', 'audio', 'sticker'], async (ctx) => {
   try {
-    const forwarded = await ctx.telegram.forwardMessage(
+    await ctx.telegram.forwardMessage(
       CHANNEL_ID,
       ctx.chat.id,
       ctx.message.message_id
     );
 
     const randomId = generateRandomId();
-    const caption = ctx.message.caption || "";
 
     let file_id = null;
-    let file_name = "Unknown";
-    let file_size = "Unknown";
 
-    if (ctx.message.document) {
-      file_id = ctx.message.document.file_id;
-      file_name = ctx.message.document.file_name || "Document";
-      file_size = `${(ctx.message.document.file_size / 1024).toFixed(2)} KB`;
-    } else if (ctx.message.video) {
-      file_id = ctx.message.video.file_id;
-      file_name = "Video.mp4";
-      file_size = `${(ctx.message.video.file_size / 1024).toFixed(2)} KB`;
-    } else if (ctx.message.audio) {
-      file_id = ctx.message.audio.file_id;
-      file_name = ctx.message.audio.file_name || "Audio.mp3";
-      file_size = `${(ctx.message.audio.file_size / 1024).toFixed(2)} KB`;
-    } else if (ctx.message.photo) {
-      const photo = ctx.message.photo[ctx.message.photo.length - 1];
-      file_id = photo.file_id;
-      file_name = "Photo.jpg";
-      file_size = `${(photo.file_size / 1024).toFixed(2)} KB`;
-    } else if (ctx.message.sticker) {
-      file_id = ctx.message.sticker.file_id;
-      file_name = "Sticker.webp";
-    }
+    if (ctx.message.document) file_id = ctx.message.document.file_id;
+    else if (ctx.message.video) file_id = ctx.message.video.file_id;
+    else if (ctx.message.audio) file_id = ctx.message.audio.file_id;
+    else if (ctx.message.photo) file_id = ctx.message.photo[ctx.message.photo.length - 1].file_id;
+    else if (ctx.message.sticker) file_id = ctx.message.sticker.file_id;
 
     fileStorage[randomId] = {
       file_id,
-      file_name,
-      file_size,
-      caption
+      caption: ctx.message.caption
     };
 
     const fileLink = `https://t.me/${ctx.botInfo.username}?start=${randomId}`;
 
     await ctx.replyWithHTML(
-      `✅ <b>Your file has been saved!</b>\n\n` +
+      `✅ Your file has been saved!\n\n` +
       `<b>Link:</b> <a href="${fileLink}">${fileLink}</a>`
     );
   } catch (err) {
@@ -107,7 +77,7 @@ bot.on("message", async (ctx) => {
   ctx.reply("Invalid command. Use /start to begin.");
 });
 
-// Web server for Render
+// Express server for Render
 app.get("/", (req, res) => {
   res.send("Bot is running!");
 });
